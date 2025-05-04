e6f0303381733a83015d8d7cab9f7d2a54fddbff
cyber06917 <lalitkumarcyber069@gmail.com>
2025-05-04 11:04:24 +0530

    no changes

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    Product: str
+    Price: float
+
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-
+@app.post("/cart")
+def get_cart(item: Item):
+    print(item)
+    return {f"payload: {item}"}
 
 
 
